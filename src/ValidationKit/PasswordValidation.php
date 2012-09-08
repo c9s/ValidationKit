b9167778bb76a9d19ac9a0cf63a4ad310feee44c
c9s <cornelius.howl@gmail.com>
2012-09-08 11:29:50 +0800

    use invalid accessor

--- src/ValidationKit/PasswordValidation.php
+++ src/ValidationKit/PasswordValidation.php
@@ -7,25 +7,24 @@ class PasswordValidation extends Validator
     {
         if( $maxLength = $this->getOption('max_length') ) {
             if( strlen( $givenPassword ) > $maxLength ) {
-                return $this->saveResult( false, 'exceed_max_length' );
+                return $this->invalid('exceed_max_length');
             }
         }
         if( $minLength = $this->getOption('min_length') ) {
             if( strlen( $givenPassword ) < $minLength )  {
-                return $this->saveResult( false, 'require_min_length' );
+                return $this->invalid('require_min_length');
             }
         }
 
         if( $this->getOption('with_digits') ) {
             if( ! preg_match( '/\d/', $givenPassword ) ) 
-                return $this->saveResult( false, 'require_digits' );
+                return $this->invalid('require_digits');
         }
 
         if( $this->getOption('with_alpha') ) {
             if( ! preg_match( '/[a-zA-Z]/' , $givenPassword ) ) 
-                return $this->saveResult( false, 'require_alpha' );
+                return $this->invalid('require_alpha');
         }
-
         return $this->saveResult( true, 'valid' );
     }
 }
