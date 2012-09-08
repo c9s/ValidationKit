a3d590860eda4f635962aa58501effc6b53f8a3b
c9s <cornelius.howl@gmail.com>
2012-09-08 14:20:43 +0800

    refactor pattern validator

--- src/ValidationKit/PatternValidator.php
+++ src/ValidationKit/PatternValidator.php
@@ -8,7 +8,11 @@ class PatternValidator extends Validator
 
     public function validate($value) 
     {
-        if( $this->saveResult( preg_match( $this->getOption('pattern') , $value , $matches ,$this->getOption('flags') ) ) ) {
+        if( preg_match( $this->getOption('pattern'), 
+                $value, 
+                $matches,
+                $this->getOption('flags') ) ) 
+        {
             $this->matches = $matches;
             return $this->valid();
         }
