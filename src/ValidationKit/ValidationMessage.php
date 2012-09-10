c9c1dded4a5741ca9cd0a013d2123e6e0b6d4427
c9s <cornelius.howl@gmail.com>
2012-09-10 10:36:59 +0800

    Add ValidationMessage class

---
+++ src/ValidationKit/ValidationMessage.php
@@ -0,0 +1,36 @@
+<?php
+namespace ValidationKit;
+
+class ValidationMessage
+{
+    public $valid = true;
+
+    /**
+     * @var string validation message id (validation result type)
+     */
+    public $id; 
+
+    /**
+     * @var string validation message
+     */
+    public $message;
+
+    public static function createValid($msgId,$msg) {
+        $msg = new self;
+        $msg->id = $msgId;
+        $msg->message = $msg;
+        $msg->valid = true;
+        return $msg;
+    }
+
+    public static function createInvalid($msgId,$msg) {
+        $msg = new self;
+        $msg->id = $msgId;
+        $msg->message = $msg;
+        $msg->valid = false;
+        return $msg;
+    }
+
+}
+
+
